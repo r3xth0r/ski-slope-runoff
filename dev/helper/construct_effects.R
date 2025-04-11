51cb3b487a888b70e1ffde992462102eceb5009a
Matthias Schlögl <matthias.schloegl@boku.ac.at>
2025-04-11 17:47:41 +0200

    REF: move helper function to dedicated script

---
+++ dev/helper/construct_effects.R
@@ -0,0 +1,17 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# construct feature effects from learner
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+construct_effects <- function(learner_nonski, learner_ski, feature) {
+  predictor_nonski <- Predictor$new(learner_nonski, data = dat_nonski, y = "psi_intervall")
+  predictor_ski <- Predictor$new(learner_ski, data = dat_ski, y = "psi_intervall")
+  effect_nonski <- FeatureEffect$new(predictor_nonski,
+    feature = feature,
+    method = "pdp+ice"
+  )
+  effect_ski <- FeatureEffect$new(predictor_ski,
+    feature = feature,
+    method = "pdp+ice"
+  )
+  list(nonski = effect_nonski, ski = effect_ski)
+}
--- dev/plots/pdp_plots.R
+++ dev/plots/pdp_plots.R
@@ -13,19 +13,7 @@ learner_ski <- readRDS("dat/interim/random_forest/ranger_trained_ski.rds")
 learner_nonski <- readRDS("dat/interim/random_forest/ranger_trained_noski.rds")
 
 # Helper function
-construct_effects <- function(learner_nonski, learner_ski, feature) {
-  predictor_nonski <- Predictor$new(learner_nonski, data = dat_nonski, y = "psi_intervall")
-  predictor_ski <- Predictor$new(learner_ski, data = dat_ski, y = "psi_intervall")
-  effect_nonski <- FeatureEffect$new(predictor_nonski,
-    feature = feature,
-    method = "pdp+ice"
-  )
-  effect_ski <- FeatureEffect$new(predictor_ski,
-    feature = feature,
-    method = "pdp+ice"
-  )
-  list(nonski = effect_nonski, ski = effect_ski)
-}
+source("dev/helper/construct_effects.R")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 # FEATURE GROUP: Land use
