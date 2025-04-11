d2e28de9144d4ed14954de18f9db115f949cb6f6
Matthias Schlögl <matthias.schloegl@boku.ac.at>
2025-04-11 20:52:35 +0200

    REF: use consistent names, native pipe, colors from theme

--- dev/plots/06_importance_plot.R
+++ dev/plots/06_importance_plot.R
@@ -6,26 +6,27 @@ library("tidyverse")
 library("patchwork")
 
 source("dev/helper/get_importance.R")
+source("dev/helper/theme_ski.R")
 
-# non-ski-slopes ----
-learner_ski <- read_rds("dat/interim/random_forest/ranger_trained_noski.rds")
-imp <- get_importance(learner_ski) |>
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Reference slopes ----
+learner_ref <- read_rds("dat/interim/random_forest/ranger_trained_noski.rds")
+
+imp <- get_importance(learner_ref) |>
   mutate(importance = if_else(importance < 0, 0, importance)) |>
-  mutate(index = gsub("_", " ", index))
-
-# Rename factor level
-imp <- imp %>%
+  mutate(index = gsub("_", " ", index)) |>
   mutate(index = fct_recode(index, "soil structure" = "embedded rock type"))
 
 # Highlight top 6 features
-top6_noski <- imp %>%
-  top_n(6, importance) %>%
+top6_ref <- imp |>
+  top_n(6, importance) |>
   pull(index)
-imp <- imp %>%
-  mutate(color = if_else(index %in% top6_noski, "highlight", "normal"))
+imp <- imp |>
+  mutate(color = if_else(index %in% top6_ref, "highlight", "normal"))
 
 # Classify point types
-imp <- imp %>%
+imp <- imp |>
   mutate(point_type = case_when(
     index %in% c("pasture", "vegetation class", "ground cover") ~ "land use",
     index %in% c("geomorphon", "slope") ~ "topography",
@@ -33,7 +34,7 @@ imp <- imp %>%
     index %in% c("soil structure", "humus type", "bulk density", "soil depth", "sd delta", "soil class") ~ "soil"
   ))
 
-p_noski <- ggplot(
+p_ref <- ggplot(
   imp,
   aes(
     x = reorder(index, importance), y = importance * 100,
@@ -44,7 +45,7 @@ p_noski <- ggplot(
 ) +
   geom_col(width = 0.1) +
   geom_point(aes(color = point_type, fill = color), size = 6, stroke = 2) +
-  scale_fill_manual(values = c("highlight" = "#A27146", "normal" = "#D3D3D3"), guide = "none") +
+  scale_fill_manual(values = c("highlight" = ref_col, "normal" = "#D3D3D3"), guide = "none") +
   scale_color_manual(values = c("land use" = "black", "topography" = "black", "geology" = "black", "soil" = "black"), guide = "none") +
   scale_shape_manual(
     values = c("land use" = 21, "topography" = 22, "geology" = 23, "soil" = 24),
@@ -67,25 +68,25 @@ p_noski <- ggplot(
   ) +
   guides(shape = guide_legend(title = NULL))
 
-# ski-slopes ----
-learner_ski <- readRDS("dat/interim/random_forest/ranger_trained_ski.rds")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Ski slopes ----
+learner_ski <- read_rds("dat/interim/random_forest/ranger_trained_ski.rds")
+
 imp <- get_importance(learner_ski) |>
   mutate(importance = if_else(importance < 0, 0, importance)) |>
-  mutate(index = gsub("_", " ", index))
-
-# Rename factor level
-imp <- imp %>%
+  mutate(index = gsub("_", " ", index)) |>
   mutate(index = fct_recode(index, "soil structure" = "embedded rock type"))
 
 # Highlight top 6 features
-top6_ski <- imp %>%
-  top_n(6, importance) %>%
+top6_ski <- imp |>
+  top_n(6, importance) |>
   pull(index)
-imp <- imp %>%
+imp <- imp |>
   mutate(color = if_else(index %in% top6_ski, "highlight", "normal"))
 
 # Classify point types
-imp <- imp %>%
+imp <- imp |>
   mutate(point_type = case_when(
     index %in% c("pasture", "vegetation class", "ground cover") ~ "land use",
     index %in% c("geomorphon", "slope") ~ "topography",
@@ -104,7 +105,7 @@ p_ski <- ggplot(
 ) +
   geom_col(width = 0.1) +
   geom_point(aes(color = point_type, fill = color), size = 6, stroke = 2) +
-  scale_fill_manual(values = c("highlight" = "#33ccff", "normal" = "#D3D3D3"), guide = "none") +
+  scale_fill_manual(values = c("highlight" = ski_col, "normal" = "#D3D3D3"), guide = "none") +
   scale_color_manual(values = c("land use" = "black", "topography" = "black", "geology" = "black", "soil" = "black"), guide = "none") +
   scale_shape_manual(
     values = c("land use" = 21, "topography" = 22, "geology" = 23, "soil" = 24),
@@ -127,8 +128,10 @@ p_ski <- ggplot(
   ) +
   guides(shape = guide_legend(title = NULL))
 
-# patchwork ----
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+# Compose plot ----
 patchwork <- (p_ski) / (p_noski)
-patchwork
-ggsave("plt/fig6.png", patchwork, device = png, height = 28, width = 30, dpi = 300, units = "cm")
+ggsave("plt/fig_06.png", patchwork, device = png, height = 28, width = 30, dpi = 300, units = "cm")
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
