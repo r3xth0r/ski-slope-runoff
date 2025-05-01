7003beff0a22a56eca319aa1670518fdabb755ca
schidli <christian.scheidl@boku.ac.at>
2025-05-01 15:18:03 +0200

    DEV: New function for long label in geo class

--- dev/helper/theme_ski.R
+++ dev/helper/theme_ski.R
@@ -15,13 +15,12 @@ theme_ski <- function(...) {
     )
   return(th)
 }
-
 theme_pdp <- function(...) {
   th <- theme_bw(...) +
     theme(
       text = element_text(size = 12),
-      axis.title.x = element_text(vjust = 0.5, size = 12),
-      axis.title.y = element_text(vjust = 0.5, size = 12),
+      axis.title.x = element_text(vjust = 0.5, size = 8),
+      axis.title.y = element_text(vjust = 0.5, size = 8),
       plot.title = element_text(
         size = rel(1), hjust = 0.5,
         margin = margin(t = 10, b = 20, unit = "pt")
@@ -31,4 +30,20 @@ theme_pdp <- function(...) {
       panel.grid.minor = element_line(size = 0.15)
     )
   return(th)
+}  
+theme_pdpgeo <- function(...) {
+    th <- theme_bw(...) +
+      theme(
+        text = element_text(size = 11),
+        axis.title.x = element_text(vjust = 0.5, size = 8),
+        axis.title.y = element_text(vjust = 0.5, size = 8),
+        plot.title = element_text(
+          size = rel(1), hjust = 0.5,
+          margin = margin(t = 10, b = 20, unit = "pt")
+        ),
+        legend.position = "none",
+        panel.grid.major = element_line(size = 0.3),
+        panel.grid.minor = element_line(size = 0.15)
+      )
+    return(th)
 }
