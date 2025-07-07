2d757781bcf026477c5e4fd4e4a463bb89317766
Matthias Schlögl <matthias.schloegl@boku.ac.at>
2025-07-07 09:37:38 +0200

    DEV: use consistent labels, remove comments, set seed for jitter

--- dev/plots/04_psi_combined.R
+++ dev/plots/04_psi_combined.R
@@ -26,15 +26,14 @@ psi_ref <- dat_ref |>
 dat_psi <- bind_rows(psi_ski, psi_ref)
 
 p <- ggplot(dat_psi, aes(x = type, y = psi_intervall, fill = as.factor(type))) +
-  geom_violin(alpha = 0.35, color = NA, width = 0.5) + # Fill the violin shapes with color
-  geom_boxplot(width = 0.1, fill = NA, color = "black") + # Add boxplot with no fill and black outline
-  geom_point(aes(color = as.factor(type)), alpha = 1.3, shape = 1, position = position_jitter(width = 0.1, height = 0), size = 1.5) + # Minimal vertical jitter, no horizontal jitter
-  scale_x_discrete(labels = c("ski slope", "reference")) +
+  geom_violin(alpha = 0.35, color = NA, width = 0.5) +
+  geom_boxplot(width = 0.1, fill = NA, color = "black") +
+  geom_point(aes(color = as.factor(type)), alpha = 1.3, shape = 1, position = position_jitter(width = 0.1, height = 0, seed = 42), size = 1.5) +
+  scale_x_discrete(labels = c("ski slope", "reference slope")) +
   scale_y_continuous(limits = c(0, 1.2), breaks = scales::breaks_extended(n = 7)) +
-  scale_fill_manual(values = c(ski_col, ref_col), labels = c("ski slope", "reference"), name = "") +
-  scale_color_manual(values = c(ski_col, ref_col), labels = c("ski slope", "reference"), name = "") +
-  labs(x = "", y = bquote(italic(C[constant]))) + # Add y-axis label
-  # coord_flip() +  # Rotate the plot 90°
+  scale_fill_manual(values = c(ski_col, ref_col), labels = c("ski slope", "reference slope"), name = "") +
+  scale_color_manual(values = c(ski_col, ref_col), labels = c("ski slope", "reference slope"), name = "") +
+  labs(x = "", y = bquote(italic(C[const.]))) +
+  # coord_flip() +
   theme_ski()
-
 ggsave("plt/fig_04.png", plot = p, device = png, height = 78.9, width = 140, dpi = 300, units = "mm")
