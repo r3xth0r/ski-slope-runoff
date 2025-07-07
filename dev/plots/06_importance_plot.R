8859ba6365b89e92c0cedb122eb32eea97f2f39c
Matthias Schlögl <matthias.schloegl@boku.ac.at>
2025-07-07 10:37:13 +0200

    REF: wrap processing and plotting in functions

--- dev/plots/06_importance_plot.R
+++ dev/plots/06_importance_plot.R
@@ -10,151 +10,89 @@ source("dev/helper/theme_ski.R")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-# Reference slopes ----
-learner_ref <- read_rds("dat/interim/random_forest/ranger_trained_noski.rds")
-
-imp <- get_importance(learner_ref) |>
-  mutate(importance = if_else(importance < 0, 0, importance)) |>
-  mutate(index = gsub("_", " ", index)) |>
-  mutate(index = fct_recode(index, "coarse fraction embedment" = "embedded rock type")) |>
-  mutate(index = fct_recode(index, "saturation deficit difference" = "sd delta")) |>
-  mutate(index = fct_recode(index, "geological class" = "geol class")) |>
-  mutate(index = fct_recode(index, "texture" = "soiltexture")) |>
-  mutate(index = fct_recode(index, "humustype" = "humus type")) |>
-  mutate(index = fct_recode(index, "vegetation" = "vegetation class"))
-
-# Highlight top 6 features
-top6_ref <- imp |>
-  top_n(5, importance) |>
-  pull(index)
-imp <- imp |>
-  mutate(color = if_else(index %in% top6_ref, "highlight", "normal"))
-
-# Classify point types
-imp <- imp |>
-  mutate(point_type = case_when(
-    index %in% c("pasture", "vegetation", "ground cover") ~ "land use",
-    index %in% c("geomorphon", "slope") ~ "topography",
-    index %in% c("skeleton", "geological class", "texture") ~ "geology",
-    index %in% c("coarse fraction embedment", "humustype", "bulk density", "soil depth", "saturation deficit difference", "soil class") ~ "soil"
-  ))
-
-p_ref <- ggplot(
-  imp,
-  aes(
-    x = reorder(index, importance), y = importance * 100,
-    label = round(100 * importance, 0),
-    fill = color,
-    shape = point_type
-  )
-) +
-  geom_col(width = 0.1) +
-  geom_point(aes(color = point_type, fill = color), size = 3, stroke = 1) +
-  scale_fill_manual(values = c("highlight" = ref_col, "normal" = "#D3D3D3"), guide = "none") +
-  scale_color_manual(
-    values = c("land use" = "black", "topography" = "black", "geology" = "black", "soil" = "black"),
-    guide = "none"
-  ) +
-  scale_shape_manual(
-    values = c("land use" = 21, "topography" = 22, "geology" = 23, "soil" = 24),
-    breaks = c("land use", "topography", "geology", "soil")
-  ) +
-  coord_flip() +
-  xlab("") +
-  scale_y_continuous(
-    name = expression(Importance ~ on ~ italic(C[constant])),
-    breaks = seq(0, 4, by = 1), limits = c(0, 4)
-  ) +
-  labs(title = paste0("reference")) +
-  theme_minimal() +
-  theme(
-    text = element_text(size = 12),
-    axis.title.y = element_text(angle = 90, vjust = 0.5, size = 12),
-    axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5),
-    axis.title.x = element_text(vjust = 0.5, size = 12),
-    axis.title.y.right = element_text(angle = 0, vjust = 0.5),
-    legend.position = "inside",
-    legend.justification.inside = c(1, 0.1),
-    legend.margin = margin(0, 0, 0, 0),
-    legend.background = element_rect(colour = "white"),
-    legend.key.size = unit(7, "mm"), # Adjust the size of the legend keys
-    legend.spacing.y = unit(3, "mm") # Adjust the spacing between legend entries
-  ) +
-  guides(shape = guide_legend(title = NULL))
+process_importance <- function(learner, top_n_features = 6L) {
+  imp <- learner |>
+    get_importance() |>
+    mutate(importance = if_else(importance < 0, 0, importance)) |>
+    mutate(index = gsub("_", " ", index)) |>
+    mutate(index = fct_recode(index,
+      "coarse fraction embedment" = "embedded rock type",
+      "saturation deficit difference" = "sd delta",
+      "geological class" = "geol class",
+      "texture" = "soiltexture",
+      "humustype" = "humus type",
+      "vegetation" = "vegetation class"
+    ))
+  top_n <- imp |>
+    top_n(top_n_features, importance) |>
+    pull(index)
+  imp |>
+    mutate(color = if_else(index %in% top_n, "highlight", "normal")) |>
+    mutate(point_type = case_when(
+      index %in% c("pasture", "vegetation", "ground cover") ~ "land use",
+      index %in% c("geomorphon", "slope") ~ "topography",
+      index %in% c("skeleton", "geological class", "texture") ~ "geology",
+      index %in% c(
+        "coarse fraction embedment", "humustype", "bulk density",
+        "soil depth", "saturation deficit difference", "soil class"
+      ) ~ "soil"
+    ))
+}
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
+plot_importance <- function(imp, title, highlight_color) {
+  ggplot(
+    imp,
+    aes(
+      x = reorder(index, importance), y = importance * 100,
+      label = round(100 * importance, 0),
+      fill = color,
+      shape = point_type
+    )
+  ) +
+    geom_col(width = 0.1) +
+    geom_point(aes(color = point_type, fill = color), size = 3, stroke = 1) +
+    scale_fill_manual(values = c("highlight" = highlight_color, "normal" = "#D3D3D3"), guide = "none") +
+    scale_color_manual(
+      values = c("land use" = "black", "topography" = "black", "geology" = "black", "soil" = "black"),
+      guide = "none"
+    ) +
+    scale_shape_manual(
+      values = c("land use" = 21, "topography" = 22, "geology" = 23, "soil" = 24),
+      breaks = c("land use", "topography", "geology", "soil")
+    ) +
+    coord_flip() +
+    xlab("") +
+    scale_y_continuous(
+      name = expression(Importance ~ on ~ italic(C[const.])),
+      breaks = seq(0, 4, by = 1), limits = c(0, 4)
+    ) +
+    labs(title = title) +
+    theme_minimal() +
+    theme(
+      text = element_text(size = 12),
+      legend.position = "inside",
+      legend.justification.inside = c(1, 0.1),
+      legend.margin = margin(0, 0, 0, 0),
+      legend.background = element_rect(color = "white"),
+      legend.key.size = unit(7, "mm"), # Adjust the size of the legend keys
+      legend.spacing.y = unit(3, "mm") # Adjust the spacing between legend entries
+    ) +
+    guides(shape = guide_legend(title = NULL))
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+# Reference slopes ----
+learner_ref <- read_rds("dat/interim/random_forest/ranger_trained_noski.rds")
+imp_ref <- process_importance(learner_ref, 6)
+p_ref <- plot_importance(imp_ref, "reference slope", ref_col)
+
 # Ski slopes ----
 learner_ski <- read_rds("dat/interim/random_forest/ranger_trained_ski.rds")
-
-imp <- get_importance(learner_ski) |>
-  mutate(importance = if_else(importance < 0, 0, importance)) |>
-  mutate(index = gsub("_", " ", index)) |>
-  mutate(index = fct_recode(index, "coarse fraction embedment" = "embedded rock type")) |>
-  mutate(index = fct_recode(index, "saturation deficit difference" = "sd delta")) |>
-  mutate(index = fct_recode(index, "geological class" = "geol class")) |>
-  mutate(index = fct_recode(index, "texture" = "soiltexture")) |>
-  mutate(index = fct_recode(index, "humustype" = "humus type")) |>
-  mutate(index = fct_recode(index, "vegetation" = "vegetation class"))
-
-# Highlight top 6 features
-top6_ski <- imp |>
-  top_n(6, importance) |>
-  pull(index)
-imp <- imp |>
-  mutate(color = if_else(index %in% top6_ski, "highlight", "normal"))
-
-# Classify point types
-imp <- imp |>
-  mutate(point_type = case_when(
-    index %in% c("pasture", "vegetation", "ground cover") ~ "land use",
-    index %in% c("geomorphon", "slope") ~ "topography",
-    index %in% c("skeleton", "geological class", "texture") ~ "geology",
-    index %in% c("coarse fraction embedment", "humustype", "bulk density", "soil depth", "saturation deficit difference", "soil class") ~ "soil"
-  ))
-
-p_ski <- ggplot(
-  imp,
-  aes(
-    x = reorder(index, importance), y = importance * 100,
-    label = round(100 * importance, 0),
-    fill = color,
-    shape = point_type
-  )
-) +
-  geom_col(width = 0.1) +
-  geom_point(aes(color = point_type, fill = color), size = 3, stroke = 1) +
-  scale_fill_manual(values = c("highlight" = ski_col, "normal" = "#D3D3D3"), guide = "none") +
-  scale_color_manual(
-    values = c("land use" = "black", "topography" = "black", "geology" = "black", "soil" = "black"),
-    guide = "none"
-  ) +
-  scale_shape_manual(
-    values = c("land use" = 21, "topography" = 22, "geology" = 23, "soil" = 24),
-    breaks = c("land use", "topography", "geology", "soil")
-  ) +
-  coord_flip() +
-  xlab("") +
-  scale_y_continuous(
-    name = expression(Importance ~ on ~ italic(C[constant])),
-    breaks = seq(0, 4, by = 1), limits = c(0, 4)
-  ) +
-  labs(title = paste0("ski slope")) +
-  theme_minimal() +
-  theme(
-    text = element_text(size = 12),
-    axis.title.y = element_text(angle = 90, vjust = 0.5, size = 12),
-    axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5),
-    axis.title.x = element_text(vjust = 0.5, size = 12),
-    axis.title.y.right = element_text(angle = 0, vjust = 0.5),
-    legend.position = "inside",
-    legend.justification.inside = c(1, 0.1),
-    legend.margin = margin(0, 0, 0, 0),
-    legend.background = element_rect(colour = "white"),
-    legend.key.size = unit(7, "mm"), # Adjust the size of the legend keys
-    legend.spacing.y = unit(3, "mm") # Adjust the spacing between legend entries
-  ) +
-  guides(shape = guide_legend(title = NULL))
+imp_ski <- process_importance(learner_ski, 6)
+p_ski <- plot_importance(imp_ski, "ski slope", ski_col)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
