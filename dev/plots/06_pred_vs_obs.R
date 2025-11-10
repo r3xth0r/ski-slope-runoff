86834929a52d635e13110585e04decee6c913ebe
Matthias Schlögl <matthias.schloegl@geosphere.at>
2025-11-10 17:19:22 +0100

    DEV: plot mod vs obs on resampled data

--- dev/plots/06_pred_vs_obs.R
+++ dev/plots/06_pred_vs_obs.R
@@ -6,7 +6,11 @@ suppressPackageStartupMessages({
   library("mlr3")
   library("data.table")
   library("dplyr")
+  library("ggplot2")
 })
+
+source("dev/helper/theme_ski.R")
+source("dev/helper/config.R")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
@@ -33,5 +37,24 @@ get_score <- function(rr, id) {
 
 s_ski <- get_score(r_ski, "ski slopes")
 s_ref <- get_score(r_ref, "reference areas")
+
+metrics <- bind_rows(s_ski, s_ref) |>
+  mutate(id = forcats::fct_relevel(id, "ski slopes", "reference areas"))
+
+p <- ggplot(metrics, aes(x = response, y = truth, color = id)) +
+  geom_smooth(method = "lm", formula = "y ~ x") +
+  geom_point(alpha = 0.5, show.legend = FALSE) +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
+  facet_wrap(~id) +
+  scale_x_continuous(name = expression(predicted ~ italic(C[const.]))) +
+  scale_y_continuous(name = expression(observed ~ italic(C[const.]))) +
+  scale_color_manual(values = c(ski_col, ref_col)) +
+  coord_fixed(xlim = c(0, 1.15), ylim = c(0, 1.15)) +
+  theme_ski()
+ggsave(
+  glue::glue("plt/fig_mod_obs_rr.{file_format}"),
+  plot = p, device = device,
+  height = 80, width = 140, units = "mm", dpi = dpi
+)
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
