55c5a9c8a3e382083e61637304c0dbdb314f9cfa
Matthias Schlögl <matthias.schloegl@boku.ac.at>
2026-02-09 19:29:01 +0100

    DEV: fill NA with 0
    check: NA are only present at the beginning and end of a time series, and next to an actual zero

--- dev/plots/A1_hydrographs.R
+++ dev/plots/A1_hydrographs.R
@@ -18,6 +18,7 @@ df_rel_psi <- df_ski_hydro |>
   arrange(timestamp) |>
   mutate(time_rel = as.numeric(difftime(timestamp, min(timestamp), units = "mins"))) |>
   ungroup() |>
+  mutate(psi_int = replace_na(psi_int, 0)) |>
   filter(time_rel <= 90)
 
 annotation_pos_y <- max(df_rel_psi$psi_int, na.rm = TRUE) * 0.95
