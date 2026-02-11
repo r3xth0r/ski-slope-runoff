r3xth0r/ski-slope-runoff

commit f6da1f02917e6af08f483ac0da3f06d2c47a92ea

Update 03_hydrograph_example.R
STY: quasi-staedy runoff

dev/plots/03_hydrograph_example.R
@@ -19,29 +19,42 @@ sz <- 3.5
 p <- ggplot(data = dat, aes(x = t, y = AK, group = type)) +
   geom_smooth(method = "loess", se = FALSE, span = 0.15, color = "black", formula = "y ~ x") +
   geom_vline(xintercept = 0, linetype = "dotted", color = "black", linewidth = 0.5) +
-  geom_segment(aes(x = 50, xend = 60, y = 0.77, yend = 0.77), linetype = "solid", color = c1, linewidth = 0.7) +
-  geom_segment(aes(x = 50, y = 0.70, xend = 50, yend = 1), linewidth = 0.5, color = "black", linetype = "dotted") +
-  geom_segment(aes(x = 60, y = 0.70, xend = 60, yend = 1), linewidth = 0.5, color = "black", linetype = "dotted") +
+  geom_segment(aes(x = 50, xend = 60, y = 0.77, yend = 0.77),
+               linetype = "solid", color = c1, size = 0.7) +
+  geom_segment(aes(x = 50, y = 0.70, xend = 50, yend = 1),
+               size = 0.5, color = "black", linetype = "dotted") +
+  geom_segment(aes(x = 60, y = 0.70, xend = 60, yend = 1),
+               size = 0.5, color = "black", linetype = "dotted") +
   geom_segment(aes(x = 55, y = 0.845, xend = 57.5, yend = 0.78),
-    linewidth = 0.2, color = c1, arrow = arrow(length = unit(1.5, "mm"), type = "closed")
-  ) +
-  annotate("text", x = 55, y = 0.89, label = expression(italic(C[const.])), size = 4, color = c1, family = font) +
+               size = 0.2, color = c1,
+               arrow = arrow(length = grid::unit(1.5, "mm"), type = "closed")) +
+  annotate("text", x = 55, y = 0.89,
+           label = "italic(C[const.])", parse = TRUE,
+           size = 4, color = c1, family = font) +
   geom_segment(aes(x = 3.4, y = 0.20, xend = 4, yend = 0.03),
-    linewidth = 0.2, color = c2, arrow = arrow(length = unit(1.5, "mm"), type = "closed")
-  ) +
-  annotate("text", x = 3.5, y = 0.30, label = bquote(italic(atop("start", "time"))), size = sz, color = c2, family = font) +
+               size = 0.2, color = c2,
+               arrow = arrow(length = grid::unit(1.5, "mm"), type = "closed")) +
+  annotate("text", x = 3.5, y = 0.30,
+           label = "italic(atop('start','time'))", parse = TRUE,
+           size = sz, color = c2, family = font) +
   geom_segment(aes(x = 0.5, y = 0.86, xend = 27.5, yend = 0.86),
-    linewidth = 0.4, color = c2, arrow = arrow(length = unit(1.7, "mm"), ends = "both", type = "closed")
-  ) +
-  annotate("text", x = 13.5, y = 0.89, label = bquote(italic("accumulation time")), size = sz, color = c2, family = font) +
+               size = 0.4, color = c2,
+               arrow = arrow(length = grid::unit(1.7, "mm"), ends = "both", type = "closed")) +
+  annotate("text", x = 13.5, y = 0.89,
+           label = "italic('accumulation time')", parse = TRUE,
+           size = sz, color = c2, family = font) +
   geom_segment(aes(x = 50.5, y = 0.73, xend = 59.5, yend = 0.73),
-    linewidth = 0.4, color = c2, arrow = arrow(length = unit(1.7, "mm"), ends = "both", type = "closed")
-  ) +
-  annotate("text", x = 55, y = 0.58, label = bquote(italic(atop("time of constant", "discharge"))), size = sz, color = c2, family = font) +
+               size = 0.4, color = c2,
+               arrow = arrow(length = grid::unit(1.7, "mm"), ends = "both", type = "closed")) +
+  annotate("text", x = 55, y = 0.58,
+           label = "italic(atop('time of quasi-steady','runoff'))", parse = TRUE,
+           size = sz, color = c2, family = font) +
   geom_segment(aes(x = 11, y = 0.4, xend = 8.3, yend = 0.43),
-    linewidth = 0.2, color = c2, arrow = arrow(length = unit(1.5, "mm"), type = "closed")
-  ) +
-  annotate("text", x = 14, y = 0.35, label = bquote(italic(atop("rising", "limb"))), size = sz, color = c2, family = font) +
+               size = 0.2, color = c2,
+               arrow = arrow(length = grid::unit(1.5, "mm"), type = "closed")) +
+  annotate("text", x = 14, y = 0.35,
+           label = "italic(atop('rising','limb'))", parse = TRUE,
+           size = sz, color = c2, family = font) +
   xlab(expression(italic(t) ~ "[min]")) +
   ylab(expression(italic(C) ~ " [-]")) +
   scale_y_continuous(limits = c(0, 1.2), breaks = scales::breaks_extended(n = 7), expand = c(0, 0)) +

commit 8842c705f6e38a0291dbc6b8a4bb2d42d23dbdbc

REV: keep only change in C_const annotation
This reverts regressions in commit f6da1f02917e6af08f483ac0da3f06d2c47a92ea.

dev/plots/03_hydrograph_example.R
@@ -19,60 +19,29 @@ sz <- 3.5
 p <- ggplot(data = dat, aes(x = t, y = AK, group = type)) +
   geom_smooth(method = "loess", se = FALSE, span = 0.15, color = "black", formula = "y ~ x") +
   geom_vline(xintercept = 0, linetype = "dotted", color = "black", linewidth = 0.5) +
-  geom_segment(aes(x = 50, xend = 60, y = 0.77, yend = 0.77),
-    linetype = "solid", color = c1, size = 0.7
-  ) +
-  geom_segment(aes(x = 50, y = 0.70, xend = 50, yend = 1),
-    size = 0.5, color = "black", linetype = "dotted"
-  ) +
-  geom_segment(aes(x = 60, y = 0.70, xend = 60, yend = 1),
-    size = 0.5, color = "black", linetype = "dotted"
-  ) +
+  geom_segment(aes(x = 50, xend = 60, y = 0.77, yend = 0.77), linetype = "solid", color = c1, linewidth = 0.7) +
+  geom_segment(aes(x = 50, y = 0.70, xend = 50, yend = 1), linewidth = 0.5, color = "black", linetype = "dotted") +
+  geom_segment(aes(x = 60, y = 0.70, xend = 60, yend = 1), linewidth = 0.5, color = "black", linetype = "dotted") +
   geom_segment(aes(x = 55, y = 0.845, xend = 57.5, yend = 0.78),
-    size = 0.2, color = c1,
-    arrow = arrow(length = grid::unit(1.5, "mm"), type = "closed")
+    linewidth = 0.2, color = c1, arrow = arrow(length = unit(1.5, "mm"), type = "closed")
   ) +
-  annotate("text",
-    x = 55, y = 0.89,
-    label = "italic(C[const.])", parse = TRUE,
-    size = 4, color = c1, family = font
-  ) +
+  annotate("text", x = 55, y = 0.89, label = expression(italic(C[const.])), size = 4, color = c1, family = font) +
   geom_segment(aes(x = 3.4, y = 0.20, xend = 4, yend = 0.03),
-    size = 0.2, color = c2,
-    arrow = arrow(length = grid::unit(1.5, "mm"), type = "closed")
+    linewidth = 0.2, color = c2, arrow = arrow(length = unit(1.5, "mm"), type = "closed")
   ) +
-  annotate("text",
-    x = 3.5, y = 0.30,
-    label = "italic(atop('start','time'))", parse = TRUE,
-    size = sz, color = c2, family = font
-  ) +
+  annotate("text", x = 3.5, y = 0.30, label = bquote(italic(atop("start", "time"))), size = sz, color = c2, family = font) +
   geom_segment(aes(x = 0.5, y = 0.86, xend = 27.5, yend = 0.86),
-    size = 0.4, color = c2,
-    arrow = arrow(length = grid::unit(1.7, "mm"), ends = "both", type = "closed")
+    linewidth = 0.4, color = c2, arrow = arrow(length = unit(1.7, "mm"), ends = "both", type = "closed")
   ) +
-  annotate("text",
-    x = 13.5, y = 0.89,
-    label = "italic('accumulation time')", parse = TRUE,
-    size = sz, color = c2, family = font
-  ) +
+  annotate("text", x = 13.5, y = 0.89, label = bquote(italic("accumulation time")), size = sz, color = c2, family = font) +
   geom_segment(aes(x = 50.5, y = 0.73, xend = 59.5, yend = 0.73),
-    size = 0.4, color = c2,
-    arrow = arrow(length = grid::unit(1.7, "mm"), ends = "both", type = "closed")
+    linewidth = 0.4, color = c2, arrow = arrow(length = unit(1.7, "mm"), ends = "both", type = "closed")
   ) +
-  annotate("text",
-    x = 55, y = 0.58,
-    label = "italic(atop('time of','quasi-steady runoff'))", parse = TRUE,
-    size = sz, color = c2, family = font
-  ) +
+  annotate("text", x = 55, y = 0.58, label = bquote(italic(atop("time of", "quasi-steady runoff"))), size = sz, color = c2, family = font) +
   geom_segment(aes(x = 11, y = 0.4, xend = 8.3, yend = 0.43),
-    size = 0.2, color = c2,
-    arrow = arrow(length = grid::unit(1.5, "mm"), type = "closed")
+    linewidth = 0.2, color = c2, arrow = arrow(length = unit(1.5, "mm"), type = "closed")
   ) +
-  annotate("text",
-    x = 14, y = 0.35,
-    label = "italic(atop('rising','limb'))", parse = TRUE,
-    size = sz, color = c2, family = font
-  ) +
+  annotate("text", x = 14, y = 0.35, label = bquote(italic(atop("rising", "limb"))), size = sz, color = c2, family = font) +
   xlab(expression(italic(t) ~ "[min]")) +
   ylab(expression(italic(C) ~ " [-]")) +
   scale_y_continuous(limits = c(0, 1.2), breaks = scales::breaks_extended(n = 7), expand = c(0, 0)) +